39b1675191b91f6d147680582049f177e36c24a6
Necdet Can Atesman <c.atesman@gmx.net>
2016-02-24 11:51:58 +0100

    Release: 0.1.3

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ with open(os.path.join(here, 'README.rst')) as f:
 
 setup(
     name='score.uwsgi',
-    version='0.1.2',
+    version='0.1.3',
     description='Management of uwsgi processes for The SCORE Framework',
     long_description=README,
     author='strg.at',
